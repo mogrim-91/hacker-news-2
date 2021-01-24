11bca365857571ea2fd91fc5ea8693ad82c0b81e
Vstranne <victorn87@gmail.com>
2021-01-24 23:00:24 +0100

    added function to retrieve posts from database, and added basic functionality to display posts on index.php

--- app/functions.php
+++ app/functions.php
@@ -6,3 +6,13 @@ function authenticated(): bool
 {
     return isset($_SESSION['loggedIn']['username']);
 }
+
+function getPosts(PDO $pdo)
+{
+    $statement = $pdo->prepare('SELECT * FROM posts');
+    $statement->execute();
+
+    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    return $posts;
+}
--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
 
+<main>
+    <?php $posts = getPosts($pdo); ?>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <h1><?php echo $post['title']; ?></h1>
+            <a href="<?php echo $post['url']; ?>"><?php echo $post['url']; ?></a>
+            <p><?php echo $post['description']; ?></p>
+        </article>
+    <?php endforeach; ?>
 
+
+</main>
 
 
 
